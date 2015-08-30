c22e28787e5bffd212191ee05624e748f41d202e
LPTK <lionel.parreaux@gmail.com>
2015-08-30 22:54:39 +0200

    More extensive benchmarks

---
+++ src/main/scala/ch/epfl/data/Benchmark.scala
@@ -0,0 +1,124 @@
+package ch.epfl.data
+
+import java.io.File
+
+import Common._
+
+import scala.collection.mutable.ArrayBuffer
+
+object Benchmark extends App {
+  import sys.process._
+  
+  case class Result(ops: Int, cakes: Int, impl: Boolean, one: Boolean, mixin: Long, erasure: Long, typer: Long, total: Long, user: Double) {
+    def toCsv = s"$ops, $cakes, $impl, $one, $mixin, $erasure, $typer, $total, $user"
+  }
+  object Result {
+    def csvHeaders = "Ops, Cakes, Impl, One, Mixin, Erasure, Typer, Total, User"
+  }
+  
+  case class Faulty(ops: Int, cakes: Int, impl: Boolean, one: Boolean, err: Throwable)
+  
+  val folder = new File("../bench")
+  
+  // ops: 85, cakes: 1, defs: 30:
+  // => "Could not write class Bench$Use1$ because it exceeds JVM code size limits. Method Bench$Ops1$T1's code too large!"
+  
+  val opsRange = 5 to 80 by 5
+  val cakesRange = 1 to 31 by 5
+  val defs = 25 // 30
+//  // Debugging:
+//  val opsRange = 5 to 5 by 5
+//  val cakesRange = 1 to 1 by 5
+//  val defs = 25
+//  // Debugging:
+//  val opsRange = 85 to 85 by 5
+//  val cakesRange = 1 to 1 by 5
+//  val defs = 25//30
+  
+  val ProcessTimes = raw"\s*(.*)\sreal\s*(.*)\suser\s*(.*)\ssys".r
+  
+  def phaseTime(name: String) = raw"\[$name in (\d+)ms\]".r
+  
+  val MixinTime = phaseTime("mixin")
+  val ErasureTime = phaseTime("erasure")
+  val TyperTime = phaseTime("typer")
+  val TotalTime = phaseTime("total")
+  
+  val totalComp = opsRange.size * cakesRange.size * 2 * 2
+  var currentComp = 0
+  
+  val errLines, outLines = new ArrayBuffer[String](1000)
+  val faults = ArrayBuffer[Faulty]()
+  
+  val ress = (for {
+    impl <- Seq(true, false)
+    one <- Seq(true, false)
+    cakes <- cakesRange
+    ops <- opsRange
+  } yield try {
+      currentComp += 1
+      println(s"$currentComp out of $totalComp")
+      
+      errLines.clear()
+      outLines.clear()
+      
+      val name = s"Bench-$impl-$one"
+      output(Code("Bench", ops, defs, cakes, implicits = impl, oneCake = one).toString, folder, name+".scala")
+      
+      s"mkdir -p ${folder}/$name-bin".! // -p: ignore if dir already exists
+      
+      var mixin, erasure, typer, total = Option.empty[Long]
+      var usr = Option.empty[Double]
+      val cmd = s"time scalac ${folder}/$name.scala -verbose -d ${folder}/$name-bin"
+      println(cmd)
+      cmd lineStream ProcessLogger(
+      ln => outLines += ln, {
+        case MixinTime(t) => mixin = Some(t.toLong)
+        case ErasureTime(t) => erasure = Some(t.toLong)
+        case TyperTime(t) => typer = Some(t.toLong)
+        case TotalTime(t) => total = Some(t.toLong)
+        case ProcessTimes(r, t, _) =>
+          println(s"Total: Scala said $total; time.real said $r")
+          usr = Some(t.toDouble)
+        case ln => errLines += ln
+      })
+      
+      val res = Result(
+        ops, cakes, impl, one,
+        mixin.get, erasure.get, typer.get, total.get,
+        usr.get
+      )
+      
+      println(res)
+      
+      Some(res)
+  } catch {
+      case e: Throwable =>
+        System.err.println("Caught "+e)
+        System.err.println("\nError lines:")
+        errLines foreach System.err.println
+        System.err.println("\nOutput lines:")
+        outLines foreach System.err.println
+        faults += Faulty(ops, cakes, impl, one, e)
+        None
+  }).flatten
+  
+  printToFile(new File(folder, "res.csv")) { p =>
+    p.println(Result.csvHeaders)
+    ress map (_.toCsv) foreach p.println
+  }
+  
+  println("Done.")
+  println("Faulty configurations:")
+  faults foreach println
+  
+  
+}
+
+
+
+
+
+
+
+
--- src/main/scala/ch/epfl/data/CakeGen.scala
+++ src/main/scala/ch/epfl/data/CakeGen.scala
@@ -4,9 +4,41 @@ import java.io.{File, PrintWriter}
 
 import ch.epfl.data.sc.pardis.utils.document._
 
+object Common {
+  def output(text: String, folder: File, fname: String) = {
+    val outputFile = new PrintWriter(new File(folder, fname))
+    outputFile.println(text)
+    outputFile.close()
+  }
+  def printToFile(f: java.io.File)(op: java.io.PrintWriter => Unit) {
+    val p = new java.io.PrintWriter(f)
+    try { op(p) } finally { p.close() }
+  }
+}
+import Common._
+
+object TestLinear extends App {
+  import sys.process._
+  
+  val folder = new File("../generated")
+  
+//  val numDefs = 5
+  for (i <- 5 to 100 by 5) {
+    val fname = s"LinTest_$i.scala"
+    output(Code("New", i, 50, 10, implicits = false).toString, folder, fname)
+    s"mkdir ${folder}/$fname-bin".!!
+    val res = s"time scalac ${folder}/$fname -d ${folder}/$fname-bin".!!
+    println(res)
+  }
+  
+}
+
 object CakeGen extends App {
-//  val (numOps, numDefs, numCakes) = (3, 2, 4)
-  val (numOps, numDefs, numCakes) = (30, 20, 40)
+//  val folder = new File("../generated")
+  val folder = new File("generated")
+  
+  val (numOps, numDefs, numCakes) = (3, 2, 4)
+//  val (numOps, numDefs, numCakes) = (30, 20, 40)
 
   val oldCode = Code("Old", numOps, numDefs, numCakes, implicits = true)
   val oldCodeSelfless = Code("OldSelfless", numOps, numDefs, numCakes, implicits = true, selfTypes = false)
@@ -15,16 +47,10 @@ object CakeGen extends App {
 
   println(oldCode)
   
-  val folder = new File("generated")
-  def output(text: String, fname: String) = {
-    val outputFile = new PrintWriter(new File(folder, fname))
-    outputFile.println(text)
-    outputFile.close()
-  }
-  output(oldCode.toString, "CakeOld.scala")
-  output(oldCodeSelfless.toString, "CakeOldSelfless.scala")
-  output(newCode.toString, "CakeNew.scala")
-  output(oneCode.toString, "CakeOne.scala")
+  output(oldCode.toString, folder, "CakeOld.scala")
+  output(oldCodeSelfless.toString, folder, "CakeOldSelfless.scala")
+  output(newCode.toString, folder, "CakeNew.scala")
+  output(oneCode.toString, folder, "CakeOne.scala")
   
   println("Done.")
 }
@@ -55,30 +81,46 @@ object Code {
           db +=\\ doc"class $tname"
           db.bracesAfter(doc"implicit class Rep$i(self: Rep[$tname])") {
             for (j <- 1 to numDefs)
-              db +=\\ doc"def m$j: Rep[Int] = ${tname}d$j(self)"
+//              db +=\\ doc"def m$j: Rep[Int] = ${tname}d$j(self)"
+              db +=\\ doc"def m$j: Rep[$tname] = ${tname}d$j(self)"
           }
           for (j <- 1 to numDefs)
-            db +=\\ doc"def ${tname}d$j(x: Rep[$tname]): Rep[Int] = lift(42)"
+//            db +=\\ doc"def ${tname}d$j(x: Rep[$tname]): Rep[Int] = lift(42)"
+            db +=\\ doc"def ${tname}d$j(x: Rep[$tname]): Rep[$tname] = x"
         }
       }
       
       if (oneCake)
         db +=\\ doc"object Cake extends $cake"
       
-      def mkCalls = for (i <- 1 to numOps; j <- 1 to numDefs) {
-        def TRep = doc"lift(new T$i)"
-        if (implicits) db +=\\ doc"$TRep.m$j"
-        else db +=\\ doc"T${i}d$j($TRep)"
-      }
+//      def mkCalls = for (i <- 1 to numOps; j <- 1 to numDefs) {
+//        def TRep = doc"lift(new T$i)"
+//        if (implicits) db +=\\ doc"$TRep.m$j"
+//        else db +=\\ doc"T${i}d$j($TRep)"
+//      }
       
+      def mkCalls() = for (i <- 1 to numOps) {
+        def TRep = doc"lift(new T$i)"
+        if (implicits) {
+          db += TRep
+          for (j <- 1 to numDefs) db += doc".m$j"
+          db.newLine
+        } else {
+          for (j <- 1 to numDefs) db += doc"T${i}d$j("
+          db += TRep
+          for (j <- 1 to numDefs) db += doc")"
+          db.newLine
+        }
+      }
+  
       for (c <- 1 to numCakes)
         if (oneCake) db.bracesAfter(doc"object Use$c") {
           db +=\\ doc"import Cake._"
-          mkCalls
+          mkCalls()
         } else db.bracesAfter(doc"class Cake$c extends " + cake) {
-          mkCalls
+          mkCalls()
         }
-    
+  
     }
     
     db.toDoc
